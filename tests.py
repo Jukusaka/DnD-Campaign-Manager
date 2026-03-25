a34712aa5b07bd0fdc356ac42fb5c26f0192f5cf
Kamil Rochala <rochalakamil2@gmail.com>
2026-03-25 19:49:02 +0100

    preparing tests file

---
+++ tests.py
@@ -0,0 +1,21 @@
+import unittest
+from pydantic import ValidationError
+from models.item import Item
+from models.spell import Spell
+from models.character import Character
+
+
+class TestItemModel(unittest.TestCase):
+    pass
+
+
+class TestSpellModel(unittest.TestCase):
+    pass
+
+
+class TestCharacterModel(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
